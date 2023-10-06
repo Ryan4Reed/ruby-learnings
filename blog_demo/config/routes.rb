f19092b228504bbe167fbc545d013a1cde179061
ryan4reed <ryan4reed@gmail.com>
2023-10-06 12:12:53 +0200

    Added comments to posts

--- blog_demo/config/routes.rb
+++ blog_demo/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # /posts/1/comments/4
+  resources :posts do
+    resources :comments
+  end
 
   # Defines the root path route ("/")
   root "pages#home"
--- blog_demo/db/seeds.rb
+++ blog_demo/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "test@gmail.com", password: '123456', password_confirmation: '123456')
+
+5.times do |x|
+    Post.create(title: "Title #{x+1}", body: "Body for post #{x+1}", user_id: User.first.id)
+end
